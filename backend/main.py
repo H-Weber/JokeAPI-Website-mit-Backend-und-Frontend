42078bf0c2c9639fe1975a3611f7b291b4183f70
H-Weber <henryxweber12@gmail.com>
2026-07-03 18:21:57 +0200

    Update main.py
    yay sprach auswahl und types

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
 CORS(app)
-
 @app.route("/api/hello")
 def hello():
-    response = requests.get("https://v2.jokeapi.dev/joke/Any?safe-mode",timeout= 5)
+    language = request.args.get("lang", "en")
+    response = requests.get(f"https://v2.jokeapi.dev/joke/Any?lang={language}&safe-mode",timeout= 5)
     data = response.json()
 
     if data['type'] == "twopart":
         return jsonify({
             "setup": data["setup"],
             "delivery": data["delivery"],
-            "twopart": True
+            "type": "twopart"
         })
-
-    return jsonify(data)
+    elif data['type'] == "sinlge":
+        return jsonify({
+            "joke": data['joke'],
+            "type": "single"
+        })
+    else:
+        return jsonify({
+            "type": "error"
+        })
+    
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
